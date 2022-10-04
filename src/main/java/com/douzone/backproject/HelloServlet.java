4b5e1162260cfd1459175feebaa847651703a43f
liverbird <ehdgns07@gmail.com>
2022-10-04 16:43:02 +0900

    end

--- src/main/java/com/douzone/backproject/HelloServlet.java
+++ src/main/java/com/douzone/backproject/HelloServlet.java
@@ -1,16 +1,26 @@
 package com.douzone.backproject;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/dev")
+@WebServlet("/dev.do")
 public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        request.setAttribute("dev", "it");
+        Writer out = response.getWriter();
+        out.write("{\"dev\" : \"it\"}");
+        System.out.println("여기 들어옴");
     }
+
+    // @Override
+    // protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    //     System.out.println("서비스 들어옴");
+    // }
 }
